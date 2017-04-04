d83fbd2478e6a8d9ac54eaa93c29d28ff1ed8f20
Naoto Kaneko <naoty.k@gmail.com>
2017-04-04 18:18:16 +0900

    Display current path at right prompt

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,3 +1,21 @@
 function fish_right_prompt
-    echo ""
+    set -l pwd_info (pwd_info "/")
+
+    echo "["
+
+    if pwd_is_home
+        echo "~/"
+    else
+        echo "/"
+    end
+
+    if test ! -z "$pwd_info[2]"
+        echo "$pwd_info[2]/"
+    end
+
+    if test ! -z "$pwd_info[1]"
+        echo "$pwd_info[1]"
+    end
+
+    echo "]"
 end
