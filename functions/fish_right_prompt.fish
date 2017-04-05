81830c586f185ace35d4e329ec143aa3a9bd7135
Naoto Kaneko <naoty.k@gmail.com>
2017-04-05 12:35:01 +0900

    Make branch name colorful

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,9 +1,16 @@
 function fish_right_prompt
     set -l pwd_info (pwd_info "/")
+    set -l color (set_color $fish_color_normal)
 
     if git_is_repo
+        set -l git_color (set_color green)
         set -l branch_name (git_branch_name)
-        echo "[$branch_name] "
+
+        if git_is_dirty
+            set git_color (set_color red)
+        end
+
+        echo "[$git_color$branch_name$color] "
     end
 
     echo "["
